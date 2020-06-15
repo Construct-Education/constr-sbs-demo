067a18e1f5c19fac23853255081c9531ec1c93e3
tashinga <mtokotashinga@gmail.com>
2020-06-15 13:03:29 +0200

    added prod files

---
+++ production/readme.md
@@ -0,0 +1,6 @@
+
+# These are copies of the files that are in Canvas in the theme editor
+
+You rarely ever need to change these files this is merely a backup and when updating them please make sure you dont neglect changing them in Canvas as well...
+
+[Theme Editor Guide](https://community.canvaslms.com/videos/1683-theme-editor-admin)
---
+++ production/script.js
@@ -0,0 +1,24 @@
+(function () {  //method from: https://community.canvaslms.com/thread/22500-mobile-javascript-development
+    // The following function will retrieve and load a JavaScript file - https://www.nczonline.net/blog/2009/07/28/the-best-way-to-load-external-javascript/
+    function loadScript(url, callback) {
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        if (script.readyState) { //IE
+            script.onreadystatechange = function () {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else { //Others
+            script.onload = function () {
+                callback();
+            };
+        }
+        script.src = url;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    }
+
+    loadScript("https://learntech.medsci.ox.ac.uk/canvas-script-repository/canvas-where-am-I/canvas-where-am-I.js", function () { 
+    });
+})();
